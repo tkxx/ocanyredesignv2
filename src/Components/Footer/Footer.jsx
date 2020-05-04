6694877fdae37df2a31162e01f6c6461fa8ec793
User <26051646+tkxx@users.noreply.github.com>
2020-05-04 03:48:01 -0400

    finished footer. 85% done with landing page, need to add triangles

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 function Footer() {
 	return (
 		<React.Fragment>
-			<section id="footer">Footer</section>
+			<footer>
+				<div>
+					<h5 id="footer-title">OCA-NEW YORK</h5>
+					<div id="footer-underline" />
+					<div class="footer-detail">
+						<ul id="footer-address">
+							<li>P.O. Box 3233</li>
+							<li>Church Street Station</li>
+							<li>New York, NY 10008</li>
+						</ul>
+
+						<p id="footer-notes">
+							OCA-NY is a 501(c)(3) organization. Our financial statements are submitted annually to the
+							IRS. All of our board members are volunteers.
+						</p>
+					</div>
+				</div>
+			</footer>
 		</React.Fragment>
 	);
 }
